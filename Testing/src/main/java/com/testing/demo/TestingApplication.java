08b46816aa250b537b156677a2c24b356ffd42d6
Ashu-029 <ashishsawale2902@gmail.com>
2023-10-27 18:01:07 +0530

    check br

--- Testing/src/main/java/com/testing/demo/TestingApplication.java
+++ Testing/src/main/java/com/testing/demo/TestingApplication.java
@@ -8,7 +8,7 @@ public class TestingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
-		System.out.println("xyz");
+		System.out.println("xyp");
 	}
 
 }
